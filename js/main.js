d5ab99ad0e7629049a5643a3ef706380c60ae4e7
Barty <bartypitt@gmail.com>
2021-02-19 21:58:16 +0000

    created inital website design

--- js/main.js
+++ js/main.js
@@ -18,28 +18,98 @@ function UpdateWindow(){
 var  deltaB = 0.1
 var xOffset = 0
 //logic
-var update = function(){
-    const positions = geometry.attributes.position.array;
-    delta = clock.getDelta();
-    deltaB += delta*(0.05 +scrollY*0.001);
-    deltaB = deltaB
 
-    const GridData = GenerateGrid(GridDimentions,deltaB);
+
+var BackgroundInfomation = {
+    speed : 0.5,
+    CameraY : 3,
+    CameraZ : 15,
+    Amplitude : 1,
+    BGColour: 0x000000,
+    BGMeshColour: 0xffffff,
+    currentDelta : 0,
+    deltaConstant : 0.1,
+    backgroundSatuation : 0,
+    meshSaturation: 1
+
+}
+
+function BackgroundUpdater(){
+    const positions = geometry.attributes.position.array
+    delta = clock.getDelta();
+    BackgroundInfomation.currentDelta += delta * BackgroundInfomation.deltaConstant
+    FinalisedDelta = BackgroundInfomation.currentDelta + 1
+
+    Camera.position.y =  BackgroundInfomation.CameraY
+    Camera.position.z =  BackgroundInfomation.CameraZ
+
+    const GridData = GenerateGrid(GridDimentions,FinalisedDelta);
     const faces = GridToMeshMap(GridDimentions);
-    const Vectors = GridToVector(GridData,deltaB);
+    const Vectors = GridToVector(GridData,BackgroundInfomation.Amplitude);
     const FaceArray = GridFlattern(Vectors , faces);
+
     FaceArray.forEach((Point,Index) => {
         if (Index%3 == 0){
-        positions[Index] = Point
+        positions[Index] = Point;
         }
     });
-    geometry.attributes.position.needsUpdate = true; // required after the first render
-    console.log(window.scrollY);
-    Camera.position.y = 4 + window.scrollY*0.005;
-    Camera.position.z =  15+ window.scrollY*0.005;
+
+    scene.fog = new THREE.Fog(BackgroundInfomation.BGColour, 1, 20); // check if you need to reinstance
+    //301934
+    scene.background.setHSL(0.8 , 0.35 , BackgroundInfomation.backgroundSatuation);
+    mesh.material.color.setHSL(0.92 , 73 , BackgroundInfomation.meshSaturation);
+    geometry.attributes.position.needsUpdate = true;
+    material.needsUpdate = true
 
 
+}
+
+var ScrollFunctions = {
+    TitlePage(CurrentValue){
+        BackgroundInfomation.CameraY = 3 + CurrentValue*5;
+        BackgroundInfomation.CameraZ = 14+ CurrentValue*3;
+        BackgroundInfomation.Amplitude = CurrentValue *3;
+        BackgroundInfomation.deltaConstant = 0.1 + window.scrollY * 0.001;
+        return
+    },
+    Pageone(CurrentValue){
+        BackgroundInfomation.backgroundSatuation = CurrentValue*0.1;
+        BackgroundInfomation.meshSaturation = 1 - CurrentValue*0.5;
+        return
+        
+    }
+}
+
+
+
+function ScrollHandler(){
+    var Position =2* window.scrollY/window.innerWidth
+    var PageNumber = Math.floor(Position);
+    var PageOffset = Position - PageNumber;
+    console.log(PageNumber)
+    switch(PageNumber){
+        case 0:
+            ScrollFunctions.TitlePage(PageOffset);
+            break;
+        case 1:
+            ScrollFunctions.Pageone(PageOffset);
+            break;
+        case 2:
+            break
+
+
+    }
+
+}
+
+
+window.addEventListener('scroll' , ScrollHandler)
+
+var update = function(){
+    BackgroundUpdater();
 };
+
+
 var render = function(){
     renderer.render(scene , Camera);
 };
@@ -111,6 +181,7 @@ function GridFlattern(Points , Faces){
 
 const canvas = document.querySelector("#BackGroundCanvas");
 var scene = new THREE.Scene();
+scene.background = new THREE.Color(0x301934);
 var Camera = new THREE.PerspectiveCamera(75 , window.innerWidth / window.innerHeight , 0.1 , 1000);
 var renderer = new THREE.WebGLRenderer({canvas});
 var clock = new THREE.Clock();
@@ -134,13 +205,13 @@ const FaceArray = GridFlattern(Vectors , faces);
 
 var geometry = new THREE.BufferGeometry();
 geometry.setAttribute( 'position', new THREE.BufferAttribute( FaceArray, 3 ) );
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff  , wireframe:true} );
+var material = new THREE.MeshBasicMaterial( { color: 0xffffff  , wireframe:true} );
 const mesh = new THREE.Mesh( geometry, material );
 
 mesh.rotation.z = Math.PI/2;
 mesh.rotation.y = 0;
 
-scene.fog = new THREE.Fog(0x000000, 1, 20);
+scene.fog = new THREE.Fog(0x301934, 1, 20);
 
 /*
 var points = Vectors.map(Vector => {
@@ -157,14 +228,6 @@ const line = new THREE.Line( geometry2, material );
 */
 
 
-
-
-
-
-
-const geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
-const material2 = new THREE.MeshBasicMaterial( {color: 0x000000} );
-const cube = new THREE.Mesh( geometry2, material2 );
 scene.add( mesh );
 Camera.position.z = 15
 Camera.position.x = -GridDimentions.Width/2
